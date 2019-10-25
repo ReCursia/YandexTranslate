0a7718a45b5db7cd58a61001b12d88b93bbb1372
SilinskyAlexander <Alexander.Silinsky@kaspersky.com>
2019-10-25 14:03:05 +0300

    decoration added

---
+++ app/src/main/java/com/recursia/yandextranslate/models/domain/WordPair.java
@@ -0,0 +1,27 @@
+package com.recursia.yandextranslate.models.domain;
+
+public class WordPair {
+    private String plainWord;
+    private String translatedWord;
+
+    public String getPlainWord() {
+        return plainWord;
+    }
+
+    public void setPlainWord(String plainWord) {
+        this.plainWord = plainWord;
+    }
+
+    public String getTranslatedWord() {
+        return translatedWord;
+    }
+
+    public void setTranslatedWord(String translatedWord) {
+        this.translatedWord = translatedWord;
+    }
+
+    public WordPair(String plainWord, String translatedWord) {
+        this.plainWord = plainWord;
+        this.translatedWord = translatedWord;
+    }
+}
