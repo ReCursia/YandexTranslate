ce6f35cfb053f0b31acb15be4614a3cd80221fcc
SilinskyAlexander <Alexander.Silinsky@kaspersky.com>
2019-10-25 14:04:25 +0300

    decoration added

---
+++ app/src/main/java/com/recursia/yandextranslate/domain/dictionary/SearchInDictionaryInteractorImpl.java
@@ -0,0 +1,14 @@
+package com.recursia.yandextranslate.domain.dictionary;
+
+import com.recursia.yandextranslate.models.domain.WordPair;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+
+public class SearchInDictionaryInteractorImpl implements SearchInDictionaryInteractor {
+    @Override
+    public Observable<List<WordPair>> searchWords(String word) {
+        return null;
+    }
+}
