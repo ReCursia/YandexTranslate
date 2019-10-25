a84116c79e3f2080559c9937d4c0397597569549
SilinskyAlexander <Alexander.Silinsky@kaspersky.com>
2019-10-25 14:03:50 +0300

    decoration added

---
+++ app/src/main/java/com/recursia/yandextranslate/domain/dictionary/AddToDictionaryInteractorImpl.java
@@ -0,0 +1,11 @@
+package com.recursia.yandextranslate.domain.dictionary;
+
+import io.reactivex.Completable;
+
+public class AddToDictionaryInteractorImpl implements AddToDictionaryInteractor {
+    //TODO add repo interfaces
+    @Override
+    public Completable addWord(String word, String fromLang, String toLang) {
+        return null;
+    }
+}
