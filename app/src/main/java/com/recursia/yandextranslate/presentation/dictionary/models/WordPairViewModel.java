1b860e98c60e01f600cc04f73ad0615cc6bc3eff
ReCursia <saha2098@yandex.ru>
2019-10-29 23:44:48 +0300

    almost done

--- app/src/main/java/com/recursia/yandextranslate/presentation/dictionary/models/WordPairViewModel.java
+++ app/src/main/java/com/recursia/yandextranslate/presentation/dictionary/models/WordPairViewModel.java
@@ -1,32 +1,7 @@
 package com.recursia.yandextranslate.presentation.dictionary.models;
 
-public class WordPairViewModel {
-    private String plainWord;
-    private String translatedWord;
-    private boolean isFavorite;
+import com.recursia.yandextranslate.domain.dictionary.models.WordPair;
 
-    public boolean isFavorite() {
-        return isFavorite;
-    }
-
-    public void setFavorite(boolean favorite) {
-        isFavorite = favorite;
-    }
-
-    public String getPlainWord() {
-        return plainWord;
-    }
-
-    public void setPlainWord(String plainWord) {
-        this.plainWord = plainWord;
-    }
-
-    public String getTranslatedWord() {
-        return translatedWord;
-    }
-
-    public void setTranslatedWord(String translatedWord) {
-        this.translatedWord = translatedWord;
-    }
+public class WordPairViewModel extends WordPair {
 
 }
